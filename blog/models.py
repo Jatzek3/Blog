51b7beca1cc0df0a5994c2791c7a7d7742c6af5d
Jacek Kawalec <jokerchocolate@gmail.com>
2020-04-07 08:42:48 +0200

    Added changes

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
