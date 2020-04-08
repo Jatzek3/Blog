caeb0ce1815b063ef0f28c126370526ec9f4d730
Jacek Kawalec <jokerchocolate@gmail.com>
2020-04-08 08:23:15 +0200

    Add: comments

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
@@ -16,3 +17,16 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name ='comments')
+    comment = models.CharField(max_length=300)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse('blog')
